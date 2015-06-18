125dc95ef2e49989dfe628691f760048289891cc
unknown <578993@BAHCND4397GQQ.resource.ds.bah.com>
2015-06-18 10:39:20 -0400

    map html,css,js

---
+++ Virtual Case Wall/map.js
@@ -0,0 +1,16 @@
+﻿var map;
+var dataResults;
+
+function initialize() {
+    map = new google.maps.Map(document.getElementById('mapdisplay'), {
+        zoom: 3,
+        center: new google.maps.LatLng(40, -187.3),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    });
+}
+
+eqfeed_callback = function (results) {
+    dataResults = results;
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);

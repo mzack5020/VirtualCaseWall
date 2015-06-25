b3910bf8b2b18b12677e6d9f421dc93a171e7ec1
curranprasad <Prasad_Curran@bah.com>
2015-06-25 15:09:37 -0400

    Google maps

--- Virtual Case Wall/pages/map/map.js
+++ Virtual Case Wall/pages/map/map.js
@@ -1,12 +1,41 @@
 ﻿var map;
 var dataResults;
+var marker;
 
 function initialize() {
+  //  personLat = 39;
+  // personLng = -77;
+
+    allLats = [39, 55, 45, 32, 88];
+    allLngs = [-77, 45, -100, 45, 30];
+
+    latSum = 0;
+    lngSum = 0;
+    meanLat = 0;
+    meanLng = 0;
+
+    for (var i = 0; i < allLats.length; i++)
+    {
+        latSum += allLats[i];
+        lngSum += allLngs[i];
+    }
+
+    meanLat = latSum / allLats.length;
+    meanLng = lngSum / allLngs.length;
+
     map = new google.maps.Map(document.getElementById('mapdisplay'), {
         zoom: 3,
-        center: new google.maps.LatLng(40, -187.3),
+        center: new google.maps.LatLng(meanLat, meanLng),
         mapTypeId: google.maps.MapTypeId.TERRAIN
     });
+
+    for (var i = 0; i < allLats.length; i++) {
+        new google.maps.Marker({ position: { lat: allLats[i], lng: allLngs[i] }, map: map });
+
+    }
+
+ 
+   // new google.maps.Marker({ position: { lat: personLat, lng: personLng }, map: map });
 }
 
 eqfeed_callback = function (results) {

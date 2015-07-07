a2aee669ab18c7e571afb884946ac56e50b4ea3d
mzack5020 <mzack5020@gmail.com>
2015-07-07 17:44:16 -0400

    Fixed

--- Virtual Case Wall/bin/Debug/AppX/pages/itemDetail/itemDetail.js
+++ Virtual Case Wall/bin/Debug/AppX/pages/itemDetail/itemDetail.js
@@ -1,10 +1,6 @@
 ﻿(function () {
     "use strict";
     console.log("itemDetail.js");
-    //why does itemDetail need these images? it probably doesnt
-	var darkGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY3B0cPoPAANMAcOba1BlAAAAAElFTkSuQmCC";
-    var lightGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY7h4+cp/AAhpA3h+ANDKAAAAAElFTkSuQmCC";
-    var mediumGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY5g8dcZ/AAY/AsAlWFQ+AAAAAElFTkSuQmCC";
 
     WinJS.UI.Pages.define("/pages/itemDetail/itemDetail.html", {
         // This function is called whenever a user navigates to this page. It
@@ -22,14 +18,14 @@
             document.getElementById("photo").style.backgroundSize = "100%"
 
             // Add details to right panel div
-          
+
             var text = "<h4>Aliases: ";
             if (person.aliases[0] != "")
-                text += person.aliases[0] + ", ";
+                text += person.aliases[0];
             if (person.aliases[1] != "")
-                text += person.aliases[1] + ", ";
+                text += ", " + person.aliases[1];
             if (person.aliases[2] != "")
-                text += person.aliases[2] + "</h4>";            
+                text += ", " + person.aliases[2] + "</h4>";
 
             text += "<h4>Sex: Male</h4>";
             text += "<h4>Race: White</h4>";
@@ -41,6 +37,40 @@
             text += "<h4>Hair: Brown</h4>";
             text += "<h4>Skintone: Light</h4>";
             document.getElementById("rightText").innerHTML = text;
+
+            text = "<h2>News Feed</h2>";
+
+            for (var i = 0; i < person.events.length; i++) {
+                switch (person.events[i].type) {
+                    case text:
+                        {
+                            text += "<h4>" + person.casenumber + " texted " + person.events[i].toValue + " from " + person.events[i].value + " on " + person.events[i].date + "</h4>";
+                            break;
+                        }
+                    case call:
+                        {
+                            text += "<h4>" + person.casenumber + " called " + person.events[i].toValue + " from " + person.events[i].value + " on " + person.events[i].date + "</h4>";
+                            break;
+                        }
+                    case email:
+                        {
+                            text += "<h4>" + person.casenumber + " emailed " + person.events[i].toValue + " from " + person.events[i].value + " on " + person.events[i].date + "</h4>";
+                            break;
+                        }
+                    case location:
+                        {
+                            text += "<h4>" + person.casenumber + " was spotted at " + person.events[i].value + " on " + person.events[i].date + "</h4>";
+                            break;
+                        }
+                    default:
+                        {
+                            text += "<h4>Uncategorized event occured - To: " + person.events[i].toValue + "; From: " + person.events[i].value + "; on " + person.events[i].date + "</h4>";
+                            break;
+                        }
+                }
+            }
+
+            document.getElementById("newsFeed").innerHTML = text;
         },
     });
 })();

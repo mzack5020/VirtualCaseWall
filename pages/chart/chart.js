26b010bc90583fea123c2238023e00956037582f
curranpr <prasadc1@tcnj.edu>
2015-07-09 17:34:00 -0400

    chart fixes

--- pages/chart/chart.js
+++ pages/chart/chart.js
@@ -6,6 +6,8 @@
     google.load('visualization', '1.0', { 'packages': ['corechart'] });
     google.setOnLoadCallback(drawLineChart);//have this call something that loads the events, then when finished does the drawLineChart
 
+    //readPersonFromPeopleFile("Richard Matt", doPersonAction);
+
     var sessionSettings = WinJS.Application.sessionState;
 
     WinJS.UI.Pages.define("/pages/chart/chart.html", {
@@ -25,36 +27,43 @@
 
         }
     });
+})();
+function drawLineChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Days');
+    data.addColumn('number', 'Events');
+    data.addRows([
+        ['Thurs 9', 30],
+        ['Wed 8', 14],
+        ['Tues 7', 25],
+        ['Mon 6', 10],
+        ['Sun 5', 14],
+        ['Sat 4', 20],
+        ['Fri 3', 42]
+           
+    ]);
 
-    function drawLineChart() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Months');
-        data.addColumn('number', 'Events');
-        data.addRows([
-            ['January', 5],
-            ['February', 20],
-            ['March', 4],
-            ['April', 3],
-            ['May', 4],
-            ['June', 5],
-            ['July', 8],
-            ['August', 20],
-            ['September', 9],
-            ['October', 8],
-            ['November', 14],
-            ['December', 25]
-        ]);
+    var properties = {
+        title: 'Latest Activity',
+        subtitle: 'July',
+        titleTextStyle: {
+            color: 'orange',
+            fontSize: 24
 
-        var properties = {
-            title: 'Activity History',
-            curveType: 'none',
-            legend: { position: 'bottom' }
-
+        },
+        vAxis: { title: "Events", 
+            titleTextStyle: {color: 'orange', fontSize:20}
+            
+         },
+        
+        curveType: 'none',
+        legend: { position: 'left' },
+        backgroundColor: '#D6D6C2'
+      
         };
 
         var plot = new google.visualization.LineChart(document.getElementById('lineChart'));
-
+  
         plot.draw(data, properties);
+
     }
-})();
-    

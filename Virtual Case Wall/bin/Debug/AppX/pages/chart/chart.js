2e4aa8d2f33d105d05259bbfef0229535a4e30fd
Ryan Pelletier <ryanmatthewpelletier@gmail.com>
2015-07-16 13:33:24 -0700

    A few chart changes like getting the day and stuff.

--- Virtual Case Wall/bin/Debug/AppX/pages/chart/chart.js
+++ Virtual Case Wall/bin/Debug/AppX/pages/chart/chart.js
@@ -24,6 +24,33 @@ window.addEventListener("message", function (e) {
 })();
 
 function drawLineChart() {
+
+    var dayMap = new Array(7);
+    dayMap[0] = "Sunday";
+    dayMap[1] = "Monday";
+    dayMap[2] = "Tuesday";
+    dayMap[3] = "Wednesday";
+    dayMap[4] = "Thursday";
+    dayMap[5] = "Friday";
+    dayMap[6] = "Saturday";
+
+    var dayFreq = {};
+    dayFreq["Sunday"] = 0;
+    dayFreq["Monday"] = 0;
+    dayFreq["Tuesday"] = 0;
+    dayFreq["Wednesday"] = 0;
+    dayFreq["Thursday"] = 0;
+    dayFreq["Friday"] = 0;
+    dayFreq["Saturday"] = 0;
+
+    console.log("COUNT  " + dayFreq["Sunday"]);
+
+    console.log(personObject);
+    for (var i = 0; i < personObject.events.length; i++) {
+        var date = new Date(personObject.events[i].date);
+        console.log(dayMap[date.getDay()]);
+    }
+
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Days');
     data.addColumn('number', 'Events');
--- Virtual Case Wall/pages/chart/chart.js
+++ Virtual Case Wall/pages/chart/chart.js
@@ -24,6 +24,35 @@ window.addEventListener("message", function (e) {
 })();
 
 function drawLineChart() {
+
+    var dayMap = new Array(7);
+    dayMap[0] = "Sunday";
+    dayMap[1] = "Monday";
+    dayMap[2] = "Tuesday";
+    dayMap[3] = "Wednesday";
+    dayMap[4] = "Thursday";
+    dayMap[5] = "Friday";
+    dayMap[6] = "Saturday";
+
+    var dayFreq = {};
+    dayFreq["Sunday"] = 0;
+    dayFreq["Monday"] = 0;
+    dayFreq["Tuesday"] = 0;
+    dayFreq["Wednesday"] = 0;
+    dayFreq["Thursday"] = 0;
+    dayFreq["Friday"] = 0;
+    dayFreq["Saturday"] = 0;
+
+    console.log("COUNT  " + dayFreq["Sunday"]);
+
+    console.log(personObject);
+    for (var i = 0; i < personObject.events.length; i++) {
+        var date = new Date(personObject.events[i].date);
+        dayFreq[dayMap[date.getDay()]] = dayFreq[dayMap[date.getDay()]] + 1;
+    }
+
+
+
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Days');
     data.addColumn('number', 'Events');
